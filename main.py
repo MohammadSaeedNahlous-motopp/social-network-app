ba3693e5f2671e90c03cf6127cd32a8afdd02988
nooraalshameri <noora.alshameri@motopp.nl>
2026-09-04 15:07:13 +0200

    Database for Posts

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from db.database import engine
+from db import database
 app = FastAPI()
 
 
 @app.get('/')
 def index():
     return {'message':"Hello World"}
+
+
+
+database.Base.metadata.create_all(engine)
---
+++ models/post.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text, Boolean
+
+from db.database import Base
+
+class DBPost(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    group_id = Column(Integer, nullable=True)
+#   group_id = Column(Integer, ForeignKey('groups.id'), nullable=True)
+
+    title = Column(String(200), nullable=False)
+
+    content = Column(Text, nullable=False )
+
+    image_url = Column(String, nullable=True)
+
+    score = Column(Integer, default=0, nullable=False)
+
+    is_visible = Column(Boolean, default=True, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
+
+
+
+
