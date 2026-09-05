86449539ee0803ae66f3d3a6736814431632e3c8
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-05 21:29:19 +0200

    Fixed error that appeared after bidirectional relationship of DBUser and DBGroup was added

--- tests/auth_tests.py
+++ tests/auth_tests.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 
 from db.hash import Hash
 from models.user import DBUser
+from models.group import DBGroup
 
 
 def create_test_image(image_format="JPEG", size=(100, 100)):
