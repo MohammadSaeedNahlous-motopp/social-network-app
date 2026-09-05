a7ee2f310441e89b3d610ff8ac3d5233e4c87b1c
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-05 21:33:14 +0200

    Fixed tests configuration. Now tests are using temporary db that are cleared before each test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,50 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
-from db.database import get_db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from db.database import Base, get_db
 from main import app
 
 
-@pytest.fixture
-def client():
-    return TestClient(app)
+SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_TEST_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 
 @pytest.fixture
 def db():
-    db: Session = next(get_db())
+    Base.metadata.create_all(bind=engine)
+
+    db: Session = TestingSessionLocal()
+
     try:
         yield db
     finally:
         db.close()
+        Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def client(db):
+    def override_get_db():
+        try:
+            yield db
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    with TestClient(app) as client:
+        yield client
+
+    app.dependency_overrides.clear()
