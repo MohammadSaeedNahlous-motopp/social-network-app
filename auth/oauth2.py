64f90e71e7e19e7bf2285cb546ba2e369114d7b3
Mohammad Saeed Nahlous <mohammadsaeednahlous@gmail.com>
2026-09-05 19:28:30 +0200

    Change OAuth's User Get Method

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -51,15 +51,15 @@ def get_current_user(
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        email: str = payload.get("sub")
+        user_id: str = payload.get("sub")
 
-        if email is None:
+        if user_id is None:
             raise credentials_exception
 
     except JWTError:
         raise credentials_exception
 
-    user_searched = user.get_user_by_email(db, email)
+    user_searched = user.get_user_by_id(db, user_id)
 
     if user_searched is None:
         raise credentials_exception
