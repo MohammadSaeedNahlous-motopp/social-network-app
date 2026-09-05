1bbc41e7ba0ba6fb16f11a330b5e82eef7efc944
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-05 22:20:04 +0200

    Removed async from group endpoints.

--- routers/group.py
+++ routers/group.py
@@ -13,37 +13,37 @@ from schemas.group import GroupView, GroupSearch, GroupBase, GroupUpdate
 router = APIRouter(prefix="/groups", tags=["groups"])
 
 @router.post("/", response_model=GroupView)
-async def create_group(request_model: GroupBase, current_user: DBUser = Depends(get_current_user), db: Session = Depends(get_db)):
-    new_group = await group.create_group(db, request_model, current_user.id)
+def create_group(request_model: GroupBase, current_user: DBUser = Depends(get_current_user), db: Session = Depends(get_db)):
+    new_group = group.create_group(db, request_model, current_user.id)
 
     return new_group
 
 @router.get("/{group_id}", response_model=GroupView)
-async def get_group_by_id(group_id: int, db: Session = Depends(get_db)):
-    searched_group = await group.get_group_by_id(db, group_id)
+def get_group_by_id(group_id: int, db: Session = Depends(get_db)):
+    searched_group = group.get_group_by_id(db, group_id)
 
     return searched_group
 
 @router.get("/search", response_model=List[GroupView])
-async def get_searched_groups(request_model: GroupSearch = Depends(), db: Session = Depends(get_db)):
+def get_searched_groups(request_model: GroupSearch = Depends(), db: Session = Depends(get_db)):
     searched_groups = group.get_groups(db, request_model)
 
     return searched_groups
 
 @router.get("/", response_model=List[GroupView])
-async def get_all_groups(db: Session = Depends(get_db)):
+def get_all_groups(db: Session = Depends(get_db)):
     all_groups = group.get_all_groups(db)
 
     return all_groups
 
 @router.put("/edit/{group_id}", response_model=GroupView)
-async def edit_group(group_id: int, request_model: GroupUpdate, db: Session = Depends(get_db), current_user: DBUser = Depends(get_current_user)):
-    updated_group = await group.update_group(db, request_model, group_id, current_user.id)
+def edit_group(group_id: int, request_model: GroupUpdate, db: Session = Depends(get_db), current_user: DBUser = Depends(get_current_user)):
+    updated_group = group.update_group(db, request_model, group_id, current_user.id)
 
     return updated_group
 
 @router.delete("/{group_id}")
-async def delete_group(group_id: int, db: Session = Depends(get_db), current_user: DBUser = Depends(get_current_user)):
+def delete_group(group_id: int, db: Session = Depends(get_db), current_user: DBUser = Depends(get_current_user)):
     group.delete_group(db, group_id, current_user.id)
 
-    return "Ok"
+    return {"Group deleted successfully"}
