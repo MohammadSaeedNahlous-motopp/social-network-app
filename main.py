894ee7ff11c38272dd193c70dfcebc72edd50b18
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-07 13:34:11 +0200

    Included roup members router to an app

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import authentication, user, group
+from routers import authentication, user, group, group_member
 from db.database import engine, Base
 
 
@@ -14,6 +14,7 @@ def index():
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(group.router)
+app.include_router(group_member.router)
 
 
 Base.metadata.create_all(bind=engine)
