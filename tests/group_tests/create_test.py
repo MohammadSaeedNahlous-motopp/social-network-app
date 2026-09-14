51426c197c45ec7ed9d655d6a78b819e706d4205
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-14 14:51:04 +0200

    Fixed broen tests after proper inmplementation of images for groups

--- tests/group_tests/create_test.py
+++ tests/group_tests/create_test.py
@@ -4,25 +4,39 @@ from sqlalchemy.orm import Session
 from models.group import DBGroup
 
 
-def test_create_group(client, db: Session, authenticated_user):
+def test_create_group(client, db: Session, authenticated_user, generate_test_image):
     # Arrange
     test_user_email = "test_user@example.com"
     test_user_name = "John Doe"
     user = authenticated_user(email=test_user_email, name=test_user_name)
 
+    group_image = generate_test_image()
+    group_background_image = generate_test_image()
+
     group_data = {
         "name": "Python Developers",
         "description": "A group for Python developers",
         "is_public": True,
+        "group_image": {"filename": "group_image.jpeg", "bytes": group_image, "data_type": "image/jpeg"},
+        "group_background_image": {"filename": "group_background_image.jpeg", "bytes": group_background_image, "data_type": "image/jpeg"},
     }
 
     # Act
     response = client.post(
         "/groups/",
-        json={
+        data={
             "name": group_data["name"],
             "description": group_data["description"],
             "is_public": group_data["is_public"],
+        },
+        files={
+            "group_img": (group_data["group_image"]["filename"],
+                          group_data["group_image"]["bytes"],
+                          group_data["group_image"]["data_type"]),
+
+            "group_background_img": (group_data["group_background_image"]["filename"],
+                        group_data["group_background_image"]["bytes"],
+                        group_data["group_background_image"]["data_type"]),
         },
     )
 
@@ -54,7 +68,7 @@ def test_create_group_unauthorized(client):
     # Act
     response = client.post(
         "/groups/",
-        json={
+        data={
             "name": group_data["name"],
             "description": group_data["description"],
             "is_public": group_data["is_public"],
