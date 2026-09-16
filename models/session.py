2f15ed519826e542aa6049e718853c8028c34a13
Moahmmad Saeed Nahlous <jay.haji041@gmail.com>
2026-09-16 10:21:13 +0200

    Session Model Added

---
+++ models/session.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timezone, timedelta
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+
+from db.database import Base
+
+
+class DBSession(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False,
+    )
+
+    session_hash = Column(
+        String,
+        nullable=False,
+        unique=True,
+        index=True,
+    )
+
+    refresh_hash = Column(
+        String,
+        nullable=False,
+        unique=True,
+        index=True,
+    )
+
+    session_expires_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc) + timedelta(minutes=30),
+    )
+
+    refresh_expires_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=30),
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc),
+    )
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
