fe75b8bc34d1eb62d82929b9231ab8fc3b8668ed
Moahmmad Saeed Nahlous <jay.haji041@gmail.com>
2026-09-16 22:16:27 +0200

    Chat Encryption Fixed

--- db/message.py
+++ db/message.py
@@ -11,7 +11,11 @@ from schemas.message import MessageCreate
 
 def create_message(request: MessageCreate, user_id: int, db: Session):
     new_message = DBMessage(
-        chat_id=request.chat_id, content=request.content, sender_id=user_id
+        chat_id=request.chat_id,
+        ciphertext=request.ciphertext,
+        nonce=request.nonce,
+        encrypted_aes_key=request.encrypted_aes_key,
+        sender_id=user_id,
     )
 
     db.add(new_message)
