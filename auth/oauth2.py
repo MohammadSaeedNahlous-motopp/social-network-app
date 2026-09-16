7853498d4a56da91d70517840d71d9246bab5651
Moahmmad Saeed Nahlous <jay.haji041@gmail.com>
2026-09-16 16:21:45 +0200

    Cleaning comments

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -2,38 +2,15 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from db import user
-from dotenv import load_dotenv
-import os
+
 
 from sqlalchemy.orm.session import Session
 
 from db.database import get_db
 from db.session import get_session_by_token
 
-load_dotenv()
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
-SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-# def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-#     to_encode = data.copy()
-#
-#     if expires_delta:
-#         expire = datetime.now(timezone.utc) + expires_delta
-#     else:
-#         expire = datetime.now(timezone.utc) + timedelta(
-#             minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-#         )
-#
-#     to_encode.update({"exp": expire})
-#
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#
-#     return encoded_jwt
 
 
 def get_current_user(
