2169b41c2b6f5205ce106de7aa80c4dfa15f3b25
Moahmmad Saeed Nahlous <jay.haji041@gmail.com>
2026-09-16 10:25:19 +0200

    Session Schemas Added

---
+++ schemas/session.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+from models.enums import ChatType
+
+
+class SessionBase(BaseModel):
+    user_id: int
+    session_hash: str | None = None
+    refresh_hash: str | None = None
+
+
+class SessionResponse(BaseModel):
+    id: int
+    user_id: int
+    session_expires_at: datetime
+    refresh_expires_at: datetime
+    created_at: datetime
+    model_config = ConfigDict(from_attributes=True)
