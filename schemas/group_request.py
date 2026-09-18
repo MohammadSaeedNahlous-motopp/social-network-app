c3b8757f7665e1b130eee6ae8a9da925a2a3fb54
Mohammad Saeed Nahlous <mohammadsaeednahlous@gmail.com>
2026-09-18 13:24:25 +0200

    Group Request Schema Added

---
+++ schemas/group_request.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, ConfigDict
+
+from models.enums import RequestStatus
+
+
+class User(BaseModel):
+    id: int
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Group(BaseModel):
+    id: int
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class GroupRequestBase(BaseModel):
+    group_id: int
+
+
+class GroupRequestDisplayBase(BaseModel):
+    id: int
+    group: Group
+    sender: User
+    status: RequestStatus
+
+    model_config = ConfigDict(from_attributes=True)
