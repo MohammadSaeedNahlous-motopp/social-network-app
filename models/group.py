79bc0eaee37613eb71881d0ae4562ed965248fcc
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-21 15:12:46 +0200

    added propertt to get tags

--- models/group.py
+++ models/group.py
@@ -29,6 +29,10 @@ class DBGroup(Base):
 
     tags = relationship("DBGroupTag", back_populates="group", cascade="all, delete-orphan")
 
+    @property
+    def tag_list(self):
+        return [group_tag.tag for group_tag in self.tags]
+
     created_at = Column(
         DateTime(timezone=True),
         nullable=False,
