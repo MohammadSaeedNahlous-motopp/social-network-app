6234363809fbdaa9f024cdcea2b506c13edda871
Ostap Ilenkiv <ostap.ilenkiv@motopp.nl>
2026-09-21 15:38:29 +0200

    Add tests for tags

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import os
 
 from cryptography.fernet import Fernet
 
+from models.group_tags import DBGroupTag
+
 # Generate a temporary master key for the test environment.
 # This means tests do not depend on the local .env file.
 os.environ["MASTER_KEY"] = Fernet.generate_key().decode()
@@ -249,3 +251,22 @@ def get_response_filename():
         return message.get_filename()
 
     return _get_response_filename
+
+
+@pytest.fixture
+def add_test_tags(db: Session):
+    def _add_test_tags(group: DBGroup, tag_ids: list[int]):
+        for tag_id in set(tag_ids):
+            db.add(
+                DBGroupTag(
+                    group_id=group.id,
+                    tag_id=tag_id,
+                )
+            )
+
+        db.commit()
+        db.refresh(group)
+
+        return group
+
+    return _add_test_tags
